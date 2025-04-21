f11db25732e289a484e6202db1db2865f9a38578
sara salari <sarasalari4609@gmail.com>
2025-04-22 00:50:26 +0330

    ezafe kardan class EightQueensGA va tabe random_solution to Scode.py

--- Scode.py
+++ Scode.py
@@ -1,6 +1,6 @@
 import random
 
 class EightQueensGA:
-     def random_solution(self):    #لیستی از موقعیت وزیرها    
+     def random_solution(self):    
         """ایجاد یک راه‌حل تصادفی."""
         return random.sample(range(self.n), self.n)
