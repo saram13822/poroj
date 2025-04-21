0e33c895f66d0735f7a3823c4436c0e360421cab
sara salari <sarasalari4609@gmail.com>
2025-04-22 01:11:15 +0330

    ezafe kardan tabe calculate_fitness va shomarsh tadokhol ha dr tabe calculate_fitness va bazgasht brazandegi dar tabe calculate_fitness to Scode.py

--- Scode.py
+++ Scode.py
@@ -4,3 +4,14 @@ class EightQueensGA:
      def random_solution(self):    
 
         return random.sample(range(self.n), self.n)
+     
+
+
+     def calculate_fitness(self, solution):
+        """محاسبه برازندگی یک راه‌حل."""
+        clashes = 0      #شمارش تداخل ها
+        for i in range(self.n):
+            for j in range(i + 1, self.n):
+                if solution[i] == solution[j] or abs(solution[i] - solution[j]) == j - i: #بررسی تداخل
+                    clashes += 1
+        return 28 - clashes  # برازندگی حداکثر 28 برای 8 وزیر
